a24cd9fe2b4f4e6ccec643b3a9396088ec35089f
Devin Yasith De Silva <57395782+DevinDeSilva@users.noreply.github.com>
2023-04-30 19:13:18 -0700

    Update independent_generator.py

--- models/independent_generator.py
+++ models/independent_generator.py
@@ -38,7 +38,7 @@ class Mapping_Net(nn.Module):
         return self.mlp(x)
 
 
-class ClassIndepedentGenerator(nn.Module):
+class Generator(nn.Module):
     
     def __init__(self, in_channels, out_channels, n_classes, t_size, mlp_dim=4, edge_importance_weighting=True, dataset='ntu', device="cpu", **kwargs):
         super().__init__()
